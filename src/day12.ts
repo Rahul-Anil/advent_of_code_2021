77bda4dcd10536297df91b1b8ef47ba8bc2668ac
Rahul Anil Damodaran <rahulanil103@gmail.com>
2022-11-21 12:03:21 +0530

    day12 TS p1

--- src/day12.ts
+++ src/day12.ts
@@ -5,23 +5,52 @@ function isSmallCave(s: string): boolean {
 }
 
 function part1(input: string): number {
-    const graph = {};
-    const nodes: string[][] = input
+    const graph: Record<string, Set<string>> = {};
+    input
         .trim()
         .split("\n")
-        .map((l) => {
-            const [from, to] = l.split("-");
+        .map((line) => {
+            const [from, to] = line.split("-");
+
             if (!graph[from]) {
-                graph[from] = [];
-            }
-            if (!graph[to]) {
-                graph[to] = [];
+                graph[from] = new Set();
             }
 
-            graph[from].push(to);
+            graph[from].add(to);
+
+            if (!graph[to]) {
+                graph[to] = new Set();
+            }
+
+            graph[to].add(from);
         });
 
-    return 1;
+    console.log(graph);
+
+    const paths: Set<string[]> = new Set();
+    const todo = [["start"]];
+
+    while (todo.length !== 0) {
+        let p = todo.pop();
+        if (p) {
+            const path: string[] = p;
+
+            if (path[path.length - 1] === "end") {
+                paths.add(path);
+                continue;
+            }
+
+            let lastNode: string = path[path.length - 1];
+            graph[lastNode].forEach((node) => {
+                if (!isSmallCave(node) || !path.includes(node)) {
+                    let newPath: string[] = [...path, node];
+                    todo.push(newPath);
+                }
+            });
+        }
+    }
+
+    return paths.size;
 }
 
 function part2(input: string): number {
