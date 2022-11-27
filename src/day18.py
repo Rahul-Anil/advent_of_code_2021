159e24fab5a10523a20d025da70b151da595722e
Rahul Anil <rahulanil101@gmail.cpm>
2022-11-27 12:55:39 +0530

    day 18 mid way solution

---
+++ src/day18.py
@@ -0,0 +1,82 @@
+class Node:
+    def __init__(self, value=None, depth=None):
+        self.value = value
+        self.parent = None
+        self.left = None
+        self.right = None
+        self.depth = depth
+
+
+class Tree:
+    def __init__(self):
+        self.root = None
+
+    def snailNumToTree(self, snailNumber: str) -> None:
+        depth = 0
+        self.root = Node(None)
+        currentNode = self.root
+        currentNode.depth = depth
+
+        for v in snailNumber:
+            if v == "[":
+                newNode = Node(None)
+                newNode.parent = currentNode
+                currentNode.left = newNode
+                depth += 1
+                newNode.depth = depth
+                currentNode = newNode
+            elif v == "]":
+                currentNode = currentNode.parent
+                depth -= 1
+            elif v.isdigit():
+                currentNode.value = int(v)
+            elif v == ",":
+                currentNode = currentNode.parent
+                depth -= 1
+                newNode = Node(None)
+                newNode.parent = currentNode
+                currentNode.right = newNode
+                depth += 1
+                newNode.depth = depth
+                currentNode = newNode
+
+    def buildSnailNumber(self, node):
+        stack = []
+        if node:
+            if node.value == None:
+                left = self.buildSnailNumber(node.left)
+                right = self.buildSnailNumber(node.right)
+                stack.append(left)
+                stack.append(right)
+            else:
+                return node.value
+        # print(f"stack: {stack}")
+        return stack
+
+    def treeToSnailNumber(self):
+        print(f"Snail Number: {self.buildSnailNumber(self.root)}")
+
+    def treeTraversal(self, curr):
+        if curr:
+            self.treeTraversal(curr.left)
+            print(f"[val: {curr.value} depth: {curr.depth}]", end="")
+            self.treeTraversal(curr.right)
+
+    def displayTree(self):
+        self.treeTraversal(self.root)
+        print(f"\n")
+
+
+def part1(input: str) -> int:
+    snailNumber = input.strip()
+    tree = Tree()
+    tree.snailNumToTree(snailNumber)
+    tree.displayTree()
+    tree.treeToSnailNumber()
+    print("\n")
+
+
+if __name__ == "__main__":
+    with open("../tests/sample_inputs/day_18_sample.txt") as f:
+        input = f.read()
+    print(f"P1: {part1(input)}")
